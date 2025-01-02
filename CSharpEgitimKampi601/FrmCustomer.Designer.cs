7157c058a245273b85e94b5b69ada9b33875d352
OKTAY AVŞAR <oktayavsar@stu.aydin.edu.tr>
2025-01-02 21:50:03 +0300

    -------

---
+++ CSharpEgitimKampi601/FrmCustomer.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CSharpEgitimKampi601
+{
+    partial class FrmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCustomerCreate = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtCustomerCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGetById.Location = new System.Drawing.Point(137, 364);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(132, 31);
+            this.btnGetById.TabIndex = 35;
+            this.btnGetById.Text = "İd\'ye göre getir";
+            this.btnGetById.UseVisualStyleBackColor = false;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(275, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 374);
+            this.dataGridView1.TabIndex = 34;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnUpdate.Location = new System.Drawing.Point(137, 327);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(132, 31);
+            this.btnUpdate.TabIndex = 33;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.Location = new System.Drawing.Point(137, 290);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(132, 31);
+            this.btnDelete.TabIndex = 32;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCustomerCreate
+            // 
+            this.btnCustomerCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCustomerCreate.Location = new System.Drawing.Point(137, 253);
+            this.btnCustomerCreate.Name = "btnCustomerCreate";
+            this.btnCustomerCreate.Size = new System.Drawing.Size(132, 31);
+            this.btnCustomerCreate.TabIndex = 31;
+            this.btnCustomerCreate.Text = "Ekle";
+            this.btnCustomerCreate.UseVisualStyleBackColor = false;
+            this.btnCustomerCreate.Click += new System.EventHandler(this.btnCustomerCreate_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnList.Location = new System.Drawing.Point(137, 216);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(132, 31);
+            this.btnList.TabIndex = 30;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtCustomerCity
+            // 
+            this.txtCustomerCity.Location = new System.Drawing.Point(137, 168);
+            this.txtCustomerCity.Name = "txtCustomerCity";
+            this.txtCustomerCity.Size = new System.Drawing.Size(132, 24);
+            this.txtCustomerCity.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 18);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Müşteri Şehir:";
+            // 
+            // txtCustomerSurname
+            // 
+            this.txtCustomerSurname.Location = new System.Drawing.Point(137, 120);
+            this.txtCustomerSurname.Name = "txtCustomerSurname";
+            this.txtCustomerSurname.Size = new System.Drawing.Size(132, 24);
+            this.txtCustomerSurname.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 18);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Müşteri Soyadı:";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(137, 69);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(132, 24);
+            this.txtCustomerName.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Müşteri Adı:";
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(137, 21);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(132, 24);
+            this.txtCustomerId.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Müşteri Id:";
+            // 
+            // FrmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 417);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCustomerCreate);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtCustomerCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCustomerSurname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmCustomer";
+            this.Text = "FrmCustomer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCustomerCreate;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtCustomerCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CSharpEgitimKampi601/FrmCustomer.cs
@@ -0,0 +1,109 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class FrmCustomer : Form
+    {
+        public FrmCustomer()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server=localhost;port=5432;Database=CustomerDb;user Id=postgres;password=1234";
+
+        void GetAllCustomers()
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "select * from customers";
+            var command= new NpgsqlCommand(query, connection);
+            var adapter= new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close();
+        }
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            GetAllCustomers();
+        }
+
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
+        {
+            string customerName=txtCustomerName.Text;
+            string customerSurname=txtCustomerSurname.Text;
+            string customerCity=txtCustomerCity.Text;
+            var connection= new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "insert into customers (CustomerName,CustomerSurname,CustomerCity) values(@customerName,@customerSurname,@customerCity)";
+            var command= new NpgsqlCommand(query, connection);
+            command.Parameters.AddWithValue("@customerName",customerName);
+            command.Parameters.AddWithValue("@customerSurname",customerSurname);
+            command.Parameters.AddWithValue("@customerCity",customerCity);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Ekleme işlemi başarılı");
+            GetAllCustomers() ;
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtCustomerId.Text);   
+            var connection= new NpgsqlConnection(connectionString); 
+            connection.Open();
+            string query = "delete from customers where customerID=@customerId";
+            var command= new NpgsqlCommand(query, connection);
+            command.Parameters.AddWithValue("@customerId",id);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Silme işlemi başarılı");
+            GetAllCustomers();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse( txtCustomerId.Text);
+            string customerName = txtCustomerName.Text;
+            string customerSurname=txtCustomerSurname.Text;
+            string customerCity=txtCustomerCity.Text;
+            var connection= new NpgsqlConnection(connectionString); 
+            connection.Open();
+            string query = "update customers set customerName=@customerName,customerSurname=@customerSurname,customerCity=@customerCity where customerId=@customerId";
+            var command= new NpgsqlCommand(query, connection);
+            command.Parameters.AddWithValue("@customerName",customerName);
+            command.Parameters.AddWithValue("@customerSurname",customerSurname);
+            command.Parameters.AddWithValue("@customerCity",customerCity);
+            command.Parameters.AddWithValue("@customerId",id);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Güncelleme işlemi başarılı");
+            GetAllCustomers();
+            
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse( txtCustomerId.Text);
+            var connection= new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "select * from customers where customerId=@customerId";
+            var command = new NpgsqlCommand(query, connection);
+            command.Parameters.AddWithValue("@customerId",id);
+            command.ExecuteNonQuery ();
+            var adapter = new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close();
+        }
+    }
+}
