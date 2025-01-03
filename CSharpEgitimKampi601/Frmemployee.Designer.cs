7f3a344e6eca7efbc75d040bbeafaf1684b49b67
OKTAY AVŞAR <oktayavsar@stu.aydin.edu.tr>
2025-01-03 16:05:45 +0300

    ----

---
+++ CSharpEgitimKampi601/Frmemployee.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace CSharpEgitimKampi601
+{
+    partial class Frmemployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCustomerCreate = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtEmployeeSalary = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmployeeSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmployeId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbEmployeeDepartment = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGetById.Location = new System.Drawing.Point(143, 398);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(132, 31);
+            this.btnGetById.TabIndex = 49;
+            this.btnGetById.Text = "İd\'ye göre getir";
+            this.btnGetById.UseVisualStyleBackColor = false;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(281, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 399);
+            this.dataGridView1.TabIndex = 48;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnUpdate.Location = new System.Drawing.Point(143, 361);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(132, 31);
+            this.btnUpdate.TabIndex = 47;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDelete.Location = new System.Drawing.Point(143, 324);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(132, 31);
+            this.btnDelete.TabIndex = 46;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCustomerCreate
+            // 
+            this.btnCustomerCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnCustomerCreate.Location = new System.Drawing.Point(143, 287);
+            this.btnCustomerCreate.Name = "btnCustomerCreate";
+            this.btnCustomerCreate.Size = new System.Drawing.Size(132, 31);
+            this.btnCustomerCreate.TabIndex = 45;
+            this.btnCustomerCreate.Text = "Ekle";
+            this.btnCustomerCreate.UseVisualStyleBackColor = false;
+            this.btnCustomerCreate.Click += new System.EventHandler(this.btnCustomerCreate_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnList.Location = new System.Drawing.Point(143, 250);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(132, 31);
+            this.btnList.TabIndex = 44;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtEmployeeSalary
+            // 
+            this.txtEmployeeSalary.Location = new System.Drawing.Point(143, 177);
+            this.txtEmployeeSalary.Name = "txtEmployeeSalary";
+            this.txtEmployeeSalary.Size = new System.Drawing.Size(132, 24);
+            this.txtEmployeeSalary.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Çalışan Maaş";
+            // 
+            // txtEmployeeSurname
+            // 
+            this.txtEmployeeSurname.Location = new System.Drawing.Point(143, 129);
+            this.txtEmployeeSurname.Name = "txtEmployeeSurname";
+            this.txtEmployeeSurname.Size = new System.Drawing.Size(132, 24);
+            this.txtEmployeeSurname.TabIndex = 41;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 18);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Çalışan Soyadı";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(143, 78);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(132, 24);
+            this.txtEmployeeName.TabIndex = 39;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Çalışan Adı:";
+            // 
+            // txtEmployeId
+            // 
+            this.txtEmployeId.Location = new System.Drawing.Point(143, 30);
+            this.txtEmployeId.Name = "txtEmployeId";
+            this.txtEmployeId.Size = new System.Drawing.Size(132, 24);
+            this.txtEmployeId.TabIndex = 37;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Çalışan id:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 18);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Çalışan Departman";
+            // 
+            // cmbEmployeeDepartment
+            // 
+            this.cmbEmployeeDepartment.FormattingEnabled = true;
+            this.cmbEmployeeDepartment.Location = new System.Drawing.Point(143, 218);
+            this.cmbEmployeeDepartment.Name = "cmbEmployeeDepartment";
+            this.cmbEmployeeDepartment.Size = new System.Drawing.Size(132, 26);
+            this.cmbEmployeeDepartment.TabIndex = 51;
+            // 
+            // Frmemployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 441);
+            this.Controls.Add(this.cmbEmployeeDepartment);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCustomerCreate);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtEmployeeSalary);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmployeeSurname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmployeId);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Frmemployee";
+            this.Text = "Frmemployee";
+            this.Load += new System.EventHandler(this.Frmemployee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCustomerCreate;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtEmployeeSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmployeeSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmployeId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbEmployeeDepartment;
+    }
+}
---
+++ CSharpEgitimKampi601/Frmemployee.cs
@@ -0,0 +1,132 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Frmemployee : Form
+    {
+        public Frmemployee()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server=localhost;port=5432;Database=CustomerDb;user Id=postgres;password=1234";
+        void EmployeeList()
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "select * from Employees";
+            var cmd= new NpgsqlCommand(query, connection);
+            var adapter= new NpgsqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);   
+            dataGridView1.DataSource = dt;
+            connection.Close();
+        }
+        void DepartmentList()
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "select * from Departments";
+            var cmd = new NpgsqlCommand(query, connection);
+            var adapter = new NpgsqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            cmbEmployeeDepartment.DisplayMember = "DepartmentName";
+            cmbEmployeeDepartment.ValueMember = "DepartmentId";
+            cmbEmployeeDepartment.DataSource = dt;
+            connection.Close();
+        }
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            EmployeeList();
+        }
+
+        private void Frmemployee_Load(object sender, EventArgs e)
+        {
+            DepartmentList();
+            EmployeeList();
+        }
+
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
+        {
+            string employeeName=txtEmployeeName.Text;
+            string employeeSurname=txtEmployeeSurname.Text;
+            int employeeSalary=int.Parse(txtEmployeeSalary.Text);
+            int departmentId = int.Parse(cmbEmployeeDepartment.SelectedValue.ToString());
+            var connection= new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "insert into employees(EmployeeName,EmployeeSurname,EmployeeSalary,DepartmentId) values(@employeeName,@employeeSurname,@employeeSalary,@DepartmentId)";
+            var cmd= new NpgsqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@employeeName",employeeName);
+            cmd.Parameters.AddWithValue("@employeeSurname",employeeSurname);
+            cmd.Parameters.AddWithValue("@employeeSalary",employeeSalary);
+            cmd.Parameters.AddWithValue("@DepartmentId",departmentId);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Personel Ekleme işlemi başarılı");
+            EmployeeList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtEmployeId.Text);
+            var connection= new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "delete from Employees where EmployeeId=@employeeId";
+            var cmd = new NpgsqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@employeeId",id);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Silme işlmei başarılı");
+            EmployeeList();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int employeeId =int.Parse( txtEmployeId.Text);
+            string employeName =txtEmployeeName.Text;
+            string employeeSurname=txtEmployeeSurname.Text;
+            int employeeSalary = int.Parse(txtEmployeeSalary.Text);
+            int departmentId = int.Parse(cmbEmployeeDepartment.SelectedValue.ToString());
+            var connection= new NpgsqlConnection(connectionString);
+            connection.Open();
+            string query = "update Employees set EmployeeName=@employeeName,EmployeeSurname=@employeeSurname,EmployeeSalary=@employeeSalary,DepartmentId=@departmentId where EmployeeId=@employeeId";
+            var cmd= new NpgsqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@employeeName",employeName);
+            cmd.Parameters.AddWithValue("@employeeSurname",employeeSurname);
+            cmd.Parameters.AddWithValue("@employeeSalary",employeeSalary);
+            cmd.Parameters.AddWithValue("@departmentId",departmentId);
+            cmd.Parameters.AddWithValue("@employeeId",employeeId);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Personel güncelleme işlemi başarılı");
+            EmployeeList();
+
+
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id =int.Parse( txtEmployeId.Text);
+            var connection= new NpgsqlConnection( connectionString);
+            connection.Open();
+            string query = "Select * from Employees where EmployeeId=@employeeId";
+            var cmd= new NpgsqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@employeeId",id);
+            var adapter = new NpgsqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close() ;
+
+        }
+    }
+}
